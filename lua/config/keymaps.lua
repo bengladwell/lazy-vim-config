1fb573d68810a719e37e72237013ab30ba3fc5ea
Ben Gladwell <bgladwell@swoop.com>
2026-02-04 13:28:47 -0500

    Add keymaps for setting spacing

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,3 +6,6 @@ vim.keymap.set("n", "<leader>yf", function()
   local relative_path = vim.fn.fnamemodify(vim.fn.expand("%:p"), ":~:.") -- get path relative to project root
   vim.fn.setreg("+", relative_path) -- copy to system clipboard
 end, { desc = "Yank file path to clipboard" })
+
+vim.keymap.set("n", "<leader>2", ":set ts=2 sw=2 expandtab<Enter>", { desc = "Set tabstop, shiftwidth to 2" })
+vim.keymap.set("n", "<leader>4", ":set ts=4 sw=4 expandtab<Enter>", { desc = "Set tabstop, shiftwidth to 4" })
